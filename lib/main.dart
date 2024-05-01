8ec6fe2f58cc1bca38fe70f41340369197483bfd
oneflutter <flutter070@gmail.com>
2024-05-01 10:33:24 +0530

    update_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:glassmorphism_demo/ui/glassmorphism_container_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,133 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GlassmorphismDemo(),
-    );
-  }
-}
-
-class GlassmorphismDemo extends StatelessWidget {
-  const GlassmorphismDemo({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "GlassMorphism",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+      title: "Glassmorphism Demo",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
         ),
-        backgroundColor: Colors.teal.shade300,
+        useMaterial3: true,
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage("https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: ListView.builder(
-            itemCount: 15,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GlassmorphismContainer(
-                  width: 300,
-                  height: 200,
-                  borderRadius: 20,
-                  blur: 10,
-                  alignment: Alignment.center,
-                  border: 2,
-                  linearGradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      const Color(0xFFffffff).withOpacity(0.1),
-                      const Color(0xFFFFFFFF).withOpacity(0.05),
-                    ],
-                    stops: const [0.1, 1],
-                  ),
-                  borderGradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      const Color(0xFFffffff).withOpacity(0.5),
-                      const Color((0xFFFFFFFF)).withOpacity(0.5),
-                    ],
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Glassmorphism Demo',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class GlassmorphismContainer extends StatelessWidget {
-  final double width, height;
-  final Widget child;
-  final double borderRadius, blur, border;
-  final Alignment alignment;
-  final LinearGradient linearGradient, borderGradient;
-
-  const GlassmorphismContainer({
-    super.key,
-    required this.width,
-    required this.height,
-    required this.child,
-    this.borderRadius = 0,
-    this.blur = 0,
-    this.border = 0,
-    this.alignment = Alignment.center,
-    required this.linearGradient,
-    required this.borderGradient,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(borderRadius),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
-        child: SizedBox(
-          width: width,
-          height: height,
-          child: Padding(
-            padding: EdgeInsets.all(border),
-            child: Container(
-              alignment: alignment,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(borderRadius),
-                gradient: linearGradient,
-                border: Border.all(
-                  width: border,
-                  color: borderGradient.colors.last.withOpacity(0.2),
-                ),
-              ),
-              child: child,
-            ),
-          ),
-        ),
-      ),
+      home: const GlassmorphismDemo(),
     );
   }
 }
---
+++ lib/ui/glass_ui_two.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GlassUIScreen extends StatefulWidget {
+  const GlassUIScreen({super.key});
+
+  @override
+  State<GlassUIScreen> createState() => _GlassUIScreenState();
+}
+
+class _GlassUIScreenState extends State<GlassUIScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
